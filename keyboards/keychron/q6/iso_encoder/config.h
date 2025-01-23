f0168e542ce0390599a5051c7755604496a662e8
Cameron Fleming <cameron@nevexo.space>
2025-01-23 22:29:15 +0000

    chore: q6: set NUM_LOCK_LED_INDEX

--- keyboards/keychron/q6/iso_encoder/config.h
+++ keyboards/keychron/q6/iso_encoder/config.h
@@ -25,6 +25,8 @@
 #define NO_PIN_NUM 10
 #define CLR_REG_VAL 0x3FF
 
+#define RGB_MATRIX_ENABLE true
+
 /* Encoder Configuration */
 #define ENCODER_DEFAULT_POS 0x3
 
@@ -32,4 +34,4 @@
 #define CAPS_LOCK_LED_INDEX 60
 
 /* Enable NUM_LOCK_LED_INDEX */
-// #define NUM_LOCK_LED_INDEX 37
+#define NUM_LOCK_LED_INDEX 37
